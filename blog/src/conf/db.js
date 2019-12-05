1158df5043291a5be47524976d24ccc6b8fca1db
Ihtml <2483690020@qq.com>
2019-12-05 23:46:38 +0800

    feat(blog): add mysql conf

---
+++ blog/src/conf/db.js
@@ -0,0 +1,15 @@
+const env = process.env.NODE_ENV // 环境参数 开发环境还是生产环境
+
+let MYSQL_CONF 
+
+if (env === 'dev') {
+    
+}
+
+if (env === 'production') {
+    
+}
+
+module.exports = {
+    MYSQL_CONF
+}
