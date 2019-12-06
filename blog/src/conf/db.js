df96d2165c1e4eb11330aa6b85172a616efaa461
Ihtml <2483690020@qq.com>
2019-12-06 22:50:22 +0800

    feat(blog): add msyql exec func
    执行mysql语句的函数

--- blog/src/conf/db.js
+++ blog/src/conf/db.js
@@ -1,9 +1,15 @@
 const env = process.env.NODE_ENV // 环境参数 开发环境还是生产环境
 
-let MYSQL_CONF 
+let MYSQL_CONF  // Mysql环境配置
 
 if (env === 'dev') {
-    
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: 'yourpassword',
+        port: '3306',
+        database: 'nodeblog'  // 相当于 use nodeblog
+    }
 }
 
 if (env === 'production') {
