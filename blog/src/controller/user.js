14b3372a2d3fe7464c73ef00a77b18b35921d553
Ihtml <2483690020@qq.com>
2019-12-10 01:29:24 +0800

    feat(blog): use mysql API user login

--- blog/src/controller/user.js
+++ blog/src/controller/user.js
@@ -1,9 +1,21 @@
+const {exec} = require('../db/mysql')
+
 const loginCheck = (username, password) => {
     // use fake data
-    if (username === 'IFE' && password === '123') {
-        return true
-    }
-    return false
+    // if (username === 'IFE' && password === '123') {
+    //     return true
+    // }
+    // return false
+    const sql = `
+        select username, realname from users
+        where username='${username}' and password='${password}'
+    `
+    console.log(username, password);
+    
+    return exec(sql).then(rows => {
+        console.log('rows: ', rows);
+        return rows[0] || {}  // select语句返回数组，只需要第一项, 如果没有，返回空对象
+    })
 }
 
 module.exports = {loginCheck}
