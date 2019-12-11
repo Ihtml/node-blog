1003449aaa63edff56f44ff5de468e13394b0eb4
Ihtml <2483690020@qq.com>
2019-12-11 23:55:00 +0800

    feat(blog): set cookie expires
    服务端设置expires控制cookie的过期时间

--- blog/app.js
+++ blog/app.js
@@ -42,6 +42,13 @@ const getPostData = (req) => {
     return promise
 }
 
+// 设置 cookie 的过期时间
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    console.log('d.toGMTString() is ', d.toGMTString())
+    return d.toGMTString()
+}
 
 const serverHandle = (req, res) => {
     res.setHeader('Content-type', 'application/json')
