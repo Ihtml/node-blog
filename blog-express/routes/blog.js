3960f8cea9ae816ee799ddd389e17ec58af24071
Ihtml <2483690020@qq.com>
2019-12-28 13:38:57 +0800

    feat(express): add blog list and detail routes
    博客列表和博客详情接口实现

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var router = express.Router(); 
-
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 // GET请求根目录的回调
 router.get('/list', function(req, res, next) {
-  res.json({
-      status: 200,
-      data: 'api/blog/list'
-  });
+    let author = req.query.author || ''
+    const keyword = req.query.keyword || ''
+
+    const result = getList(author, keyword)
+    return result.then(listData => {
+        res.json(
+            new SuccessModel(listData)
+        )
+    })
 });
 
 router.get('/detail', function(req, res, next) {
-    res.json({
-        status: 200,
-        data: 'api/blog/detail'
-    });
+    const result = getDetail(req.query.id)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
   });
 
 module.exports = router;
