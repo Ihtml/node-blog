6806ad9522a4eff2d37d415775a62991c3876afb
Ihtml <2483690020@qq.com>
2020-01-02 23:48:39 +0800

    feat(koa2): blog router and user router
    blog和user的API接口

--- blog-koa2/routes/blog.js
+++ blog-koa2/routes/blog.js
@@ -1,13 +1,81 @@
 const router = require('koa-router')()
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
-router.prefix('/api/blog') 
+router.prefix('/api/blog')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+router.get('/list', async function (ctx, next) {
+    let author = ctx.query.author || ''
+    const keyword = ctx.query.keyword || ''
+
+    if (ctx.query.isadmin) {
+        console.log('is admin')
+        // 管理员界面
+        if (ctx.session.username == null) {
+            console.error('is admin, but no login')
+            // 未登录
+            ctx.body = new ErrorModel('未登录')
+            return
+        }
+        // 强制查询自己的博客
+        author = ctx.session.username
+    }
+
+    const listData = await getList(author, keyword)
+    ctx.body = new SuccessModel(listData)
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.get('/detail', async function (ctx, next) {
+    const data = await getDetail(ctx.query.id)
+    ctx.body = new SuccessModel(data)
+})
+
+router.post('/new', loginCheck, (req, res, next) => {
+    req.body.author = req.session.username
+    const result = newBlog(req.body)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+    const result = updateBlog(req.query.id, req.body)
+    return result.then(val => {
+        if (val) {
+            res.json(
+                new SuccessModel()
+            )
+        } else {
+            res.json(
+                new ErrorModel('更新博客失败')
+            )
+        }
+    })
+})
+
+router.post('/del', loginCheck, (req, res, next) => {
+    const author = req.session.username
+    const result = delBlog(req.query.id, author)
+    return result.then(val => {
+        if (val) {
+            res.json(
+                new SuccessModel()
+            )
+        } else {
+            res.json(
+                new ErrorModel('删除博客失败')
+            )
+        }
+    })
 })
 
 module.exports = router
--- blog-koa2/routes/user.js
+++ blog-koa2/routes/user.js
@@ -1,28 +1,35 @@
 const router = require('koa-router')()
+const { login } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
-router.prefix('/api/user')  
+router.prefix('/api/user')
 
 router.post('/login', async function (ctx, next) {
-  const {username, password} = ctx.request.body
-  ctx.body = {
-      status: 200,
-      username,
-      password
-  }
+    const { username, password } = ctx.request.body
+    const data = await login(username, password)
+    if (data.username) {
+        // 设置 session
+        ctx.session.username = data.username
+        ctx.session.realname = data.realname
+
+        ctx.body = new SuccessModel('登录成功')
+        return
+    }
+    ctx.body = new ErrorModel('登录失败')
 })
 
-router.get('/session-test', async function (ctx, next) {
-  if (ctx.session.viewCount == null) {
-      ctx.session.viewCount = 0
-  }
-  ctx.session.viewCount++
-  console.log(typeof ctx.session.viewCount);
-  
+// router.get('/session-test', async function (ctx, next) {
+//     if (ctx.session.viewCount == null) {
+//         ctx.session.viewCount = 0
+//     }
+//     ctx.session.viewCount++
+//     console.log(typeof ctx.session.viewCount);
 
-  ctx.body = {
-      status: 200,
-      viewCount: ctx.session.viewCount
-  }
-})
+
+//     ctx.body = {
+//         status: 200,
+//         viewCount: ctx.session.viewCount
+//     }
+// })
 
 module.exports = router
