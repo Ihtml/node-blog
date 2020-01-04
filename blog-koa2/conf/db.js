622f305a1053382fd3068aa7397e4571e0a2a882
Ihtml <2483690020@qq.com>
2020-01-04 12:38:16 +0800

    feat(koa2): add db conf

---
+++ blog-koa2/conf/db.js
@@ -0,0 +1,39 @@
+const env = process.env.NODE_ENV // 环境参数 开发环境还是生产环境
+
+let MYSQL_CONF  // Mysql环境配置
+let REDIS_CONF // redis环境配置
+
+if (env === 'dev') {
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: 'Mysql_password',
+        port: '3306',
+        database: 'nodeblog'  // 相当于 use nodeblog
+    }
+
+    REDIS_CONF = {
+        port:  6379,
+        host: '127.0.0.1'
+    }
+}
+
+if (env === 'production') {
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: 'Mysql_password',
+        port: '3306',
+        database: 'myblog'
+    }
+
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
+    }
+}
+
+module.exports = {
+    MYSQL_CONF,
+    REDIS_CONF
+}
